fffbb2a4ba48364881510dc51f8ee7d8a4e0864f
unknown <linkinmjs@gmail.com>
2025-08-29 20:09:26 -0300

    Add some scenes

---
+++ scripts/bottom_scene.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var parallax_background: ParallaxBackground = $ParallaxBackground
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
---
+++ scripts/parrot.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	update_animation()
+	move_and_slide()
+
+
+func update_animation():
+	if is_on_floor():
+		animated_sprite_2d.play("idle")
+	elif not is_on_floor():
+		animated_sprite_2d.play("flying")
