c3c6b75ae96e1faa8ed497e8a216d01059e178e2
unknown <linkinmjs@gmail.com>
2025-08-31 15:02:11 -0300

    Add some logic on pirate script

--- scripts/pirate.gd
+++ scripts/pirate.gd
@@ -2,18 +2,21 @@ extends CharacterBody2D
 
 var action_area: Area2D
 var pirate: AnimatedSprite2D
+var cannon: AnimatedSprite2D
 var parrot_in_range: bool = false
+@export var pirate_has_cannon: bool = false
 
 
 func _ready() -> void:
-	pirate = get_node("AnimatedSprite2D")
+	pirate = get_node("Pirate")
+	if pirate_has_cannon:
+		cannon = get_node("Cannon")
 	action_area = get_node("ActionArea")
 	action_area.body_entered.connect(_on_action_area_body_entered)
 	action_area.body_exited.connect(_on_action_area_body_exited)
-	action_area.input_event.connect(_on_action_area_input_event)
 
 func _process(delta: float) -> void:
-	pass
+	update_animation()
 
 func _on_action_area_body_entered(body: Node):
 	parrot_in_range = true
@@ -23,7 +26,23 @@ func _on_action_area_body_exited(body: Node):
 	parrot_in_range = false
 	print("parrot leaving pirate")
 
-func _on_action_area_input_event(input_event: InputEvent):
-	print(input_event)
-	print("Action Pirate!!")
+func _unhandled_input(event: InputEvent) -> void:
+	## TODO: evitar solapamiento de acciones
+	if parrot_in_range and event.is_action_pressed("action"):
+		_action_event()
+		print("Action!!!")
+
+func _action_event():
 	pirate.play("action")
+	if pirate_has_cannon:
+		print("Fire!")
+		cannon.play("action")
+
+func update_animation():
+	if pirate.animation == "action" and pirate.is_playing():
+		if pirate_has_cannon and cannon.animation == "action" and cannon.is_playing():
+			return
+		return
+	pirate.play("idle")
+	if pirate_has_cannon:
+		cannon.play("idle")
