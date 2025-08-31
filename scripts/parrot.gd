66979f30692ce46a97d2bcf3567f5930a3e72843
unknown <linkinmjs@gmail.com>
2025-08-31 12:47:52 -0300

    Modify animations

--- scripts/parrot.gd
+++ scripts/parrot.gd
@@ -14,6 +14,10 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_VELOCITY
 
+	# Handle action
+	if Input.is_action_just_pressed("action"):
+		pass
+
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("left", "right")
 	if direction:
@@ -26,8 +30,15 @@ func _physics_process(delta: float) -> void:
 
 
 func update_animation():
+	
+	if animated_sprite_2d.animation == "special" and animated_sprite_2d.is_playing():
+		return
+	
+	if is_on_floor() and Input.is_action_just_pressed("action"):
+		animated_sprite_2d.play("special")
+		return
+		
 	if is_on_floor():
 		animated_sprite_2d.play("idle")
 	elif not is_on_floor():
 		animated_sprite_2d.play("flying")
-	
