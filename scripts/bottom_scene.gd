88df2c75d21dc59a12876115569793c219a320a5
unknown <linkinmjs@gmail.com>
2025-08-31 17:35:42 -0300

    Modify ship and parallax

--- scripts/bottom_scene.gd
+++ scripts/bottom_scene.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
-@onready var parallax_background: ParallaxBackground = $ParallaxBackground
-@export var speed:= 100
-
 func _ready() -> void:
 	pass
 
 
 func _process(delta: float) -> void:
-	parallax_background.scroll_base_offset.x -= speed * delta
+	pass
